da109056c2535fc1648c0f6980b12022aa348345
Jose Garcia <rocketrito1@gmail.com>
2021-10-25 15:00:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+const gifts = [];
+
+function writeCards(name, event) {
+
+    let messages = [];
+
+    for(let i = 0; i < names.length; i++) 
+
+    { messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`); }
+
+    return messages;
+}
+
+function countDown(i) {
+    while(i >= 0) 
+    { 
+        console.log(i); 
+        i --;
+    }
+
+}
